38817ae9f7b53f11dee26ff6f389398938dfc36c
Cedric Lepinay <storm.cryogen@gmail.com>
2012-05-23 21:37:33 +0000

    Support media

--- Antares/Antares/urls.py
+++ Antares/Antares/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, include, url
-
+from django.views.static import *
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     # Examples:
     # url(r'^$', 'Antares.views.home', name='home'),
     # url(r'^Antares/', include('Antares.foo.urls')),
