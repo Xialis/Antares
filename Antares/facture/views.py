7f3e153fc15f373751b269bfd5afd64c00fb90bd
Cedric Lepinay <storm.cryogen@gmail.com>
2012-06-28 20:44:38 +0000

    Utilisation du Filtrage externe et commun de app:client

--- Antares/facture/views.py
+++ Antares/facture/views.py
@@ -1,9 +1,35 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.contrib import messages
+from django.core.context_processors import csrf
 
+from client.forms import FormRechercheClient
+from client.func import initFiltration, filtration
 
 def index(request):
     c = {}
     
-    return render_to_response("facture/index.html", context_instance=RequestContext(request))
+    formRechercheClient = FormRechercheClient()
+    
+    riF = initFiltration(request)
+    if riF['b_listeFiltree']:
+        b_listeFiltree = True
+        formRechercheClient = FormRechercheClient(riF['posted'])
+    else:
+        b_listeFiltree = False
+        
+    if request.method == 'POST':
+        
+        if 'reClient' in request.POST:
+
+            retour = filtration(request)
+            b_listeFiltree = retour['b_listeFiltree']
+            if b_listeFiltree == True:
+                formRechercheClient = FormRechercheClient(request.POST)
+            else:
+                formRechercheClient = FormRechercheClient()
+    
+    c['formRechercheClient'] = formRechercheClient
+    c.update(csrf(request))
+    return render_to_response("facture/index.html", c, context_instance=RequestContext(request))
