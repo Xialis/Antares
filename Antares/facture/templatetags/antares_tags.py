05a2b516c563e761746b67f0e039f73376e46115
Cedric Lepinay <storm.cryogen@gmail.com>
2012-09-30 19:00:54 +0000

    Nouveaux custom tags

--- Antares/facture/templatetags/antares_tags.py
+++ Antares/facture/templatetags/antares_tags.py
@@ -17,3 +17,28 @@ def bt(href, aid, uiiconstyle, text):
 @register.inclusion_tag("_inc/submit-button.html")
 def sbt(formID, param, aid, uiiconstyle, text):
     return {'formID': formID, 'param': param, 'aid': aid, 'uiiconstyle': uiiconstyle, 'text': text}
+
+
+@register.inclusion_tag("_inc/j-button.html")
+def jbt(jfunc, aid, uiiconstyle, text, *args):
+
+    onclickstr = jfunc + "("
+    for arg in args:
+        try:
+            float(arg)
+        except:
+            string = "\"" + arg + "\""
+        else:
+            string = str(arg)
+
+        onclickstr += str(string) + ','
+
+    onclickstr = onclickstr.rstrip(',')
+    onclickstr += ")"
+
+    return {'jfunc': onclickstr, 'uiiconstyle': uiiconstyle, 'text': text}
+
+
+@register.inclusion_tag("_inc/formfield_errors.html")
+def ffe(errors):
+    return {'errors': errors}
