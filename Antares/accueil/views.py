533c088c9a7078ea9564eece92ad004573ca958c
Cedric Lepinay <storm.cryogen@gmail.com>
2013-01-23 22:50:53 +0000

    Authentication

---
+++ Antares/accueil/views.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from django.contrib import messages
+from django.core.context_processors import csrf
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def index(request):
+    c = {}
+    return render_to_response("accueil/index.html", c, context_instance=RequestContext(request))
