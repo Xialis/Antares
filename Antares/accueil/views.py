b525c6667eb34aef37e70afac682361f13fc71ca
Cedric Lepinay <storm.cryogen@gmail.com>
2013-03-02 15:38:56 +0000

    Nettoyage

--- Antares/accueil/views.py
+++ Antares/accueil/views.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.contrib import messages
-from django.core.context_processors import csrf
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 
 
