333f08e0e9e3efd3e05fd9a805dd133c25526103
Cedric Lepinay <storm.cryogen@gmail.com>
2013-04-01 11:10:54 +0000

    Déconnexion utilisateur

--- Antares/accueil/views.py
+++ Antares/accueil/views.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 from django.contrib import messages
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
 
 @login_required
 def index(request):
     c = {}
     return render_to_response("accueil/index.html", c, context_instance=RequestContext(request))
+
+
+def userlogout(request):
+    logout(request)
+    return redirect(index)
